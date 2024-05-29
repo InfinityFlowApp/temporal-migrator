25157990653c791715e02f52771b79d3e113d52c
Simon Novak <snovak7@gmail.com>
2024-05-29 17:28:10 +0200

    chore(#1): added logger factory other than NullLoggerFactory

--- test/Migrator/Fixtures/TemporalFixture.cs
+++ test/Migrator/Fixtures/TemporalFixture.cs
@@ -43,6 +43,7 @@ public sealed class TemporalFixture : IAsyncLifetime
                 new TemporalClientOptions
                 {
                     Namespace = "test",
+                    LoggerFactory = LoggerFactory.Create(builder => builder.AddConsole()),
                 });
     }
 
